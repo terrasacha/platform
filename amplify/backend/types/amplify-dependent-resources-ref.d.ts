8958ca6e1f76664f320254defe8fdc2b869a3484
ROBIN OCHOA <robin8a@local.mac>
2022-09-09 16:43:36 -0500

    added amplify storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s37845b3b9": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
