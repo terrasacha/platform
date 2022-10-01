6bde5cd7bcd267cf0d45413147b24a28609772fd
LoOW3 <ignaciodiaznanni@gmail.com>
2022-10-01 00:55:42 -0300

    ya puedo saber si las variables de la formula concuerdan con las features de un producto

--- src/components/Admon/Results/Results.jsx
+++ src/components/Admon/Results/Results.jsx
@@ -13,11 +13,14 @@ export default class Results extends Component {
         this.state = {
             formulas: [],
             products: [],
-            selectedFormulaID: '', 
+            selectedFormulaID: '',
+            equationSelected: '', 
             selectedProductID: '', 
+            selectedProductName: ''
 
         }
         this.handleOnChangeInputForm = this.handleOnChangeInputForm.bind(this)
+        this.checkIfVariablesMatchWithPF = this.checkIfVariablesMatchWithPF.bind(this)
     }
     componentDidMount = async () => {
         await this.loadFormulas()
@@ -37,11 +40,36 @@ export default class Results extends Component {
 
     handleOnChangeInputForm = async(e) => {
         if (e.target.name === 'result.selectedProduct') {
-        this.setState({selectedProductID: e.target.value})   
+            this.setState({selectedProductID: e.target.value})  
+            let productSelected = this.state.products.filter(product => product.id === e.target.value)
+            this.setState({selectedProductID: e.target.value, selectedProductName: productSelected[0].name })   
         }
-        
+        if (e.target.name === 'result.selectedFormula') {
+            let formulaSelected = this.state.formulas.filter(formula => formula.id === e.target.value)
+            this.setState({selectedFormulaID: e.target.value, equationSelected: formulaSelected[0].equation }) 
+            
+        }
     }
-
+    checkIfVariablesMatchWithPF = () =>{
+        let formulaCopy = this.state.equationSelected
+        let formulaCopyclean = formulaCopy.replace(/[()]/g, '').split(/[*/+-]/).filter(items => !parseInt(items)) //separa todas las variables y excluye numeros
+        let productFeaturesProductSelected = this.state.products.filter(p => p.id === this.state.selectedProductID) //eligo lasa productFeatures del producto seleccionado para usar la formula
+        productFeaturesProductSelected = productFeaturesProductSelected[0].productFeatures.items.map(pf => pf.feature.name) //lo convierto en un array con los nombres de las features para comparar con el array de formulaCopyclean
+        let formulaCopycleanLength = formulaCopyclean.length
+        let aux = 0
+        for (let i = 0;i < formulaCopyclean.length; i++){
+            if(productFeaturesProductSelected.includes(formulaCopyclean[i])) aux = aux + 1
+        }
+        if(formulaCopycleanLength === aux) {return(
+               console.log(' El producto si contiene todas las variables')
+        )
+        }else{
+            return(
+                console.log(aux),
+                console.log(' El producto no contiene todas las variables')
+            )
+         }
+    }
     render() {
         const SelectProductForm = () => {
             
@@ -62,22 +90,35 @@ export default class Results extends Component {
                     <Form.Group as={Col} controlId='formGridNewCategoryName'>
                         <Form.Label>Select a formula</Form.Label>
                         <Form.Select 
-                            name='result.selectedProduct'
+                            name='result.selectedFormula'
                             onChange={(e) => this.handleOnChangeInputForm(e)}>
                                 <option>-</option>
-                                {this.state.products.map((products, idx) => (<option value={products.id} key={idx}>{products.name}</option>))}
+                                {this.state.formulas.map((formula, idx) => (<option value={formula.id} key={idx}>{formula.equation}</option>))}
                         </Form.Select>
                     </Form.Group>
                 )
+        }
+        const CheckVariablesPF = () => {
+            if(this.state.selectedFormulaID !== '' && this.state.selectedProductID !== ''){
+                return(
+                    <>
+                        <div>Check if ProductFeatures of {this.state.selectedProductName} match with the variables on the equation</div>
+                        <Button
+                            variant='primary'
+                            size='sm' 
+                            onClick={(e) => this.checkIfVariablesMatchWithPF()}
+                        >Check</Button> 
+                    </>
+                )
+            }
         }
         return (
         <Container>
         <br></br>
         <Form>
-
                 {SelectProductForm()}   
                 {SelectFormulaForm()}
-
+                {CheckVariablesPF()}
 
 {/*             <Row className='mb-1'>
                 <Button
