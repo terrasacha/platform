13b3ef422b98242f37188d15d769b26c301d8259
EC2 Default User <ec2-user@ip-172-31-25-240.ec2.internal>
2022-10-07 18:15:44 +0000

    With blueprints

--- src/components/views/CardanoConnector/CardanoConnector.jsx
+++ src/components/views/CardanoConnector/CardanoConnector.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Tab, Tabs, RadioGroup, Radio, FormGroup, InputGroup, NumericInput } from "@blueprintjs/core";
-// import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
-// import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css";
-// import "../node_modules/normalize.css/normalize.css";
+import "../../../../node_modules/@blueprintjs/core/lib/css/blueprint.css";
+import "../../../../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "../../../../node_modules/normalize.css/normalize.css";
 import {
     Address,
     BaseAddress,
