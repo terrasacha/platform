b3e7e0df657743d1d25eb4f37628f57cedf8b670
ROBIN OCHOA <robin8a@local.mac>
2022-10-07 12:19:35 -0500

    out of memory

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import LandingPage from "./components/views/LandingPage.jsx";
 import {
   BrowserRouter, Route, Routes
 } from "react-router-dom";
+import CardanoConnector from './components/views/CardanoConnector/CardanoConnector';
 
 class App extends Component{
   
@@ -32,6 +33,7 @@ class App extends Component{
           <Route path="landing_page" element={<LandingPage />} />
           <Route path="admon" element={<Admon/>} />
           <Route path="investor_admon" element={<InvestorAdmon/>} />
+          <Route path="cardano_connector" element={<CardanoConnector/>} />
           {/* ToDd fat arrow for new Route version <Route
               path="/admon"
               render={(props) => <Admon {...props} />}
