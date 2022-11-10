dab74608569a2d0b1a13fbea6cde57767fc5f7f8
LoOW3 <ignaciodiaznanni@gmail.com>
2022-11-10 06:11:14 -0300

    .

--- src/components/views/Orders/Orders.jsx
+++ src/components/views/Orders/Orders.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import axios from "axios"
+import axios from "axios";
+import React, { Component } from 'react';
 
 export default class Orders extends Component {
     constructor(props) {
