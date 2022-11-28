35e4813d3bfe6a5eeb9208ffd566f4f8d059be27
ROBIN OCHOA <robin8a@local.mac>
2022-11-28 08:11:33 -0500

    UI constructor

---
+++ src/components/Admon/UserProducts/UserProducts.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class UserProducts extends Component {
+  render() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    Droplist Role
+                </li>
+                <li>
+                    List User base on role
+                </li>
+                <li>
+                    Products
+                </li>
+                <li>
+                    Button Link
+                </li>
+
+                <li>
+                    Table showing by user's producst linked
+                </li>
+            </ul>
+        </div>
+    )
+  }
+}
---
+++ src/components/Constructor/ConstructorAdmon.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ConstructorAdmon extends Component {
+  render() {
+    return (
+      <div>UI upload documents related to a products</div>
+    )
+  }
+}
