2dc7a024792b8833177675f316ddf7b637f6d9e8
NeiderSmith <neider.smith1@gmail.com>
2023-04-25 09:52:53 -0500

    createVerification on PF submit

--- src/components/Constructor/NewProduct/NewProduct.jsx
+++ src/components/Constructor/NewProduct/NewProduct.jsx
@@ -40,6 +40,16 @@ class NewProduct extends Component {
                 categoryID: '',
                 images: [],
             },
+            newVerification: {
+                id: '',
+                createdOn: '',
+                updatedOn: '',
+                sign: '',
+                userVerifierID: '',
+                userVerifiedID: '',
+                productFeatureID: '',
+                documentStatus: '',
+            },
             company: {
                 name: '',
                 id: '',
