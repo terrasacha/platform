529c955c9966b7ecaff383aaea6125606fde6fd2
Robin Ochoa <robin8a@gmail.com>
2023-05-02 22:50:43 -0500

    error on import

--- amplify/backend/function/kiosuanbcrjsapp58b48a41/src/index.js
+++ amplify/backend/function/kiosuanbcrjsapp58b48a41/src/index.js
@@ -14,11 +14,31 @@
 // };
 
 
-const aws = require('aws-sdk')
-const ses = new aws.SES()
+import { default as fetch, Request } from 'node-fetch';
+import { SESClient } from "@aws-sdk/client-ses";
+const ses = new SESClient({ region: "us-east-1" });
+
+const GRAPHQL_ENDPOINT = process.env.API_d6sfnut2zrdljfaz3ysbjfhvke_GRAPHQLAPIENDPOINTOUTPUT;
+const GRAPHQL_API_KEY = process.env.API_d6sfnut2zrdljfaz3ysbjfhvke_GRAPHQLAPIKEYOUTPUT;
+
+const query = /* GraphQL */ `
+  query LIST_PRODUCTS {
+    listProducts {
+      items {
+        id
+        name
+        status
+      }
+    }
+  }
+`;
+
+// const aws = require('aws-sdk')
+// const ses = new aws.SES()
 
 exports.handler = async (event) => {
-  console.log(event)
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+
   for (const streamedItem of event.Records) {
     if (streamedItem.eventName === 'MODIFY') {
       //pull off items from stream
@@ -41,5 +61,44 @@ exports.handler = async (event) => {
           .promise()
     }
   }
-  return { status: 'done' }
+
+  
+  const options = {
+    method: 'POST',
+    headers: {
+      'x-api-key': GRAPHQL_API_KEY,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query })
+  };
+
+  const request = new Request(GRAPHQL_ENDPOINT, options);
+
+  let statusCode = 200;
+  let body;
+  let response;
+
+  try {
+    response = await fetch(request);
+    body = await response.json();
+    if (body.errors) statusCode = 400;
+  } catch (error) {
+    statusCode = 400;
+    body = {
+      errors: [
+        {
+          status: response.status,
+          message: error.message,
+          stack: error.stack
+        }
+      ]
+    };
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify(body)
+  };
+
+  // return { status: 'done' }
 }
