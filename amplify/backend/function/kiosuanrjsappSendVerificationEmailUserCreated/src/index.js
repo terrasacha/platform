cc7621560889c36d2f415c08a615941a2cbf84dc
Robin Ochoa <robin8a@gmail.com>
2023-06-01 15:12:21 -0500

    not working

--- amplify/backend/function/kiosuanrjsappSendVerificationEmailUserCreated/src/index.js
+++ amplify/backend/function/kiosuanrjsappSendVerificationEmailUserCreated/src/index.js
@@ -1,30 +1,34 @@
 
-// requires
-const AWS = require('aws-sdk');
-// Set region 
-AWS.config.update({region: 'us-east-1'});
+const { SESClient, SendEmailCommand, verifyEmailIdentity } = require("@aws-sdk/client-ses");
+const ses = new SESClient({ region: "us-east-1" });
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = event => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  // Create promise and SES service object
-  var verifyEmailPromise = new AWS.SES({apiVersion: '2010-12-01'}).verifyEmailIdentity({EmailAddress: "robin8a@gmail.com"}).promise();
+exports.handler = async (event) => {
+  console.log(event)
 
-  // Handle promise's fulfilled/rejected states
-  verifyEmailPromise.then(
-    function(data) {
-      console.log("Data: ", data);
-      console.log("Email verification initiated");
-    }).catch(
-      function(err) {
-      console.error(err, err.stack);
-    });
-    // for (const record of event.Records) {
-    //   console.log(record.eventID);
-    //   console.log(record.eventName);
-    //   console.log('DynamoDB Record: %j', record.dynamodb);
-    // }
-    // return Promise.resolve('Successfully processed DynamoDB record');
-};
+  var params = {
+    EmailAddress: "robin8a@gmail.com"
+   };
+   
+  //  ses.verifyEmailIdentity(params, function(err, data) {
+  //    if (err) console.log(err, err.stack); // an error occurred
+  //    else     console.log(data);           // successful response
+  //    /*
+  //    data = {
+  //    }
+  //    */
+  //  });
+
+  const command = new verifyEmailIdentity(params);
+
+      try {
+        const data = await ses.send(command);
+        return { status: 'done', msg: data }
+      } catch (error) {
+        return { status: 'error', msg: error }
+      }
+
+  
+
+  
+  return { status: 'done' }
+}
