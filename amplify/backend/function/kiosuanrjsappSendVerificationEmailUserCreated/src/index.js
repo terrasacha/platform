eac4084ef76f890e7269ec43cd45b2f84fba89cf
Robin Ochoa <robin8a@gmail.com>
2023-06-01 17:03:20 -0500

    updated with record

--- amplify/backend/function/kiosuanrjsappSendVerificationEmailUserCreated/src/index.js
+++ amplify/backend/function/kiosuanrjsappSendVerificationEmailUserCreated/src/index.js
@@ -12,9 +12,9 @@ exports.handler = async (event) => {
     console.log(record.eventName);
     console.log('DynamoDB Record: %j', record.dynamodb);
 
-    if (streamedItem.eventName === 'INSERT') {
+    if (record.eventName === 'INSERT') {
 
-      const EMAIL_ADDRESS = streamedItem.dynamodb.NewImage.email.S;
+      const EMAIL_ADDRESS = record.dynamodb.NewImage.email.S;
       const verifyEmailIdentityCommand = new VerifyEmailIdentityCommand({ EmailAddress: EMAIL_ADDRESS });
   
       try {
@@ -27,49 +27,6 @@ exports.handler = async (event) => {
     }
   }
 
-
-  // for (const streamedItem of event.Records) {
-  //   if (streamedItem.eventName === 'INSERT') {
-
-  //     const EMAIL_ADDRESS = "robin8a@gmail.com"
-  //     const verifyEmailIdentityCommand = new VerifyEmailIdentityCommand({ EmailAddress: EMAIL_ADDRESS });
-  
-  //     try {
-  //       return await ses.send(verifyEmailIdentityCommand);
-  //     } catch (err) {
-  //       console.log("Failed to verify email identity.", err);
-  //       return err;
-  //     }
-  //   }
-  // }
-
-
-
-  // var params = {
-  //   EmailAddress: "robin8a@gmail.com"
-  //  };
-
-  //  ses.verifyEmailIdentity(params, function(err, data) {
-  //    if (err) console.log(err, err.stack); // an error occurred
-  //    else     console.log(data);           // successful response
-  //    /*
-  //    data = {
-  //    }
-  //    */
-  //  });
-
-  // const command = new verifyEmailIdentity(params);
-
-  //     try {
-  //       const data = await ses.send(command);
-  //       return { status: 'done', msg: data }
-  //     } catch (error) {
-  //       return { status: 'error', msg: error }
-  //     }
-
-  
-
-  
   return { status: 'done' }
 }
 
