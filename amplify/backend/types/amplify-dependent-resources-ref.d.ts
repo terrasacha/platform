d8fbd6ee2ead0203b531fb8efbca4273317a54bc
LoOW3 <ignaciodiaznanni@gmail.com>
2023-06-20 22:40:42 -0300

    new function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -39,6 +39,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "kiosuanbcrjsappConstructorUploadNewDocument": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "kiosuanbcrjsappSendMailNotByComment": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
