00ede6f38d11a176ca5c3f71b5aff058df617725
LoOW3 <ignaciodiaznanni@gmail.com>
2023-09-13 19:59:20 -0300

    assign validator UserProduct

--- amplify/backend/function/kiosuanbcrjsappc0136629/src/index.js
+++ amplify/backend/function/kiosuanbcrjsappc0136629/src/index.js
@@ -8,35 +8,35 @@ const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
 const GRAPHQL_API_KEY = process.env.GRAPHQL_API_KEY;
 const SES_EMAIL = process.env.SES_EMAIL
 const query = /* GraphQL */ `
-  query GetVerification($id: ID!) {
-    getVerification(id: $id) {
+query GetUserProduct($id: ID!) {
+  getUserProduct(id: $id) {
+    id
+    userID
+    user {
       id
-      userVerifier {
-        id
-        name
-        email
-      }
-      productFeature {
-        id
-        product {
-          id
-          name
-        }
-      }
+      name
+      role
+      email
+    }
+    product {
+      id
+      name
     }
   }
-`;
+}
+`
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async(event) => {
-  /* for (const record of event.Records) {
-
+  for (const record of event.Records) { 
+    /* console.log('DynamoDB Record: %j', record.dynamodb); */
+    
     if (record.eventName === 'INSERT' && record.dynamodb.NewImage) {
-      let verification = record.dynamodb.NewImage.id.S
-        const variables = { id: verification }; */
+      let userProduct = record.dynamodb.NewImage.id.S
+        const variables = { id: userProduct };
         /** @type {import('node-fetch').RequestInit} */
-    /*     const options = {
+        const options = {
           method: 'POST',
           headers: {
             'x-api-key': GRAPHQL_API_KEY, 
@@ -52,22 +52,24 @@ exports.handler = async(event) => {
         let response;
         let userVerifierEmail = ''
         let userVerifierName = ''
+        let userVerifierRole = ''
         let productName = ''
         try {
           response = await fetch(request);
           body = await response.json();
           console.log(`body ${JSON.stringify(body)}`)
-          userVerifierEmail = body.data.getVerification.userVerifier.email
-          userVerifierName = body.data.getVerification.userVerifier.name
-          productName = body.data.getVerification.productFeature.product.name
+          userVerifierEmail = body.data.getUserProduct.user.email
+          userVerifierName = body.data.getUserProduct.user.name
+          userVerifierRole = body.data.getUserProduct.user.role
+          productName = body.data.getUserProduct.product.name
           if (body.errors) statusCode = 400;
         } catch (error) {
           statusCode = 400;
           console.log(error)
         }
-        if(userVerifierEmail !== ''){
+        if(userVerifierEmail !== '' && userVerifierRole !== '' && userVerifierRole === 'validator'){
           const fromMail = SES_EMAIL
-          const toMail = [userVerifierEmail]
+          const toMail = [userVerifierEmail]  
           const data3 =   `Se te ha asignado el proyecto ${productName} para la revisión de documentación. Recuerda priorizar la asignación de valores a "Token Price", "Token Name" y "Amount of tokens" para que el proyecto pueda ser visualizado en el Marketplace`
           const templateData = {
             data: data3,
@@ -88,7 +90,9 @@ exports.handler = async(event) => {
           } catch (error) {
             return { status: 'error', msg: error }
           }
+        }else{
+          return {status: 'error', msg: 'el usuario creado no es validador'}
         }
     }
-  } */
+  }
 };
