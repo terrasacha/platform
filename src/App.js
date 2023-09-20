b983a7d5c12495e6b21a696eb5c5e11914da2b00
Robin Ochoa <robin8a@gmail.com>
2023-09-20 09:27:10 -0500

    less warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+// import React, { Component, useEffect, useState } from 'react';
 
 // Components
 import Admon from "./components/Admon/Admon";
