cad8a023b8afbf36a4a5e505f98a8af31172ce39
Jairock93 <jairojas.r12@gmail.com>
2023-10-25 22:18:57 -0500

    validacion de información completa

--- src/components/Constructor/ProjectPage/ProjectFiles/InfoCards/validateProjectFeatures.jsx
+++ src/components/Constructor/ProjectPage/ProjectFiles/InfoCards/validateProjectFeatures.jsx
@@ -1,34 +1,7 @@
 export function validateProjectFeatures(projectFeatures) {
-  const NewprojectFeatures = projectFeatures.reduce((resultado, item) => {
-    if (typeof item.value === 'string') {
-      try {
-        // Intenta analizar el valor como JSON
-        const parsedValue = JSON.parse(item.value);
-        if (Array.isArray(parsedValue)) {
-          resultado[item.featureID] = parsedValue;
-        } else {
-          resultado[item.featureID] = item.value;
-        }
-      } catch (error) {
-        // Si no es JSON válido, asigna el valor como una cadena
-        resultado[item.featureID] = item.value;
-      }
-    } else {
-      resultado[item.featureID] = item.value;
-    }
-    return resultado;
-  }, {});
-
-  // Comprobar si hay propiedades con valores vacíos - SI SI HAY VACIOS RETORNA TRUE
-  const hasEmptyValues = Object.values(NewprojectFeatures).some(value => {
-    if (typeof value === 'string') {
-      return value === '';
-    }
-    if (Array.isArray(value)) {
-      return value.length === 0;
-    }
-    return false; // Si encuentra que todos esta llenos arroja un falso
-  });
-
-  return hasEmptyValues
+  
+  const hasFinancialConditions = projectFeatures.find(item => item.featureID === 'GLOBAL_VALIDATOR_SET_FINANCIAL_CONDITIONS' && item.value === 'true');
+  const hasTechnicalConditions = projectFeatures.find(item => item.featureID === 'GLOBAL_VALIDATOR_SET_TECHNICAL_CONDITIONS' && item.value === 'true');
+  
+  return hasFinancialConditions && hasTechnicalConditions;
 }
