b3857f5625e3e167518c858f3335fb059e99c6c9
Jairock93 <jairojas.r12@gmail.com>
2024-03-10 15:53:26 -0500

    menu va!

--- src/components/Admon/Navbars/HeaderNavbar.jsx
+++ src/components/Admon/Navbars/HeaderNavbar.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Container from '../../ui/Container';
-import { Link } from 'react-router-dom';
-
 import LOGO from '../../common/_images/suan_logo.png';
 
 export default class HeaderNavbar extends Component {
@@ -28,69 +25,79 @@ export default class HeaderNavbar extends Component {
   };
 
   render() {
-    let { isActualUserLogged } = this.props;
-    let role = localStorage.getItem('role');
+    const { isActualUserLogged } = this.props;
+    const role = localStorage.getItem('role');
 
     const renderNavBar = () => {
       if (isActualUserLogged) {
         return (
-          <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <Container>
-              <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                  <img src={LOGO} alt="Logo" className="h-8" />
-                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                    suan
-                  </span>
-                </Link>
-                <button
-                  data-collapse-toggle="navbar-default"
-                  type="button"
-                  className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                  aria-controls="navbar-default"
-                  aria-expanded="false"
+          <nav className="bg-white fixed top-0 w-full">
+            <div className="container mx-auto flex items-center justify-between">
+              <a href="/" className="flex items-center">
+                <img
+                  src={LOGO}
+                  alt="ATP"
+                  className="w-auto h-10"
+                />
+              </a>
+              <button
+                className="lg:hidden focus:outline-none"
+                onClick={() => console.log('Toggle navbar')}
+              >
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <span className="sr-only">Open main menu</span>
-                  <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 17 14"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M1 1h15M1 7h15M1 13h15"
-                    />
-                  </svg>
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                  <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                      <Link
-                        to="/"
-                        className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                        aria-current="page"
-                      >
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/about"
-                        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                      >
-                        About
-                      </Link>
-                    </li>
-                    {/* Add other navigation links as needed */}
-                  </ul>
-                </div>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  ></path>
+                </svg>
+              </button>
+              <div className="hidden lg:flex items-center space-x-4">
+                <a href="#products" onClick={(e) => this.changeHeaderNavBarRequest('products', e)}>
+                  Proyectos
+                </a>
+                <a href="#documents" onClick={(e) => this.changeHeaderNavBarRequest('documents', e)}>
+                  Documentos
+                </a>
+                <a href="#formulas" onClick={(e) => this.changeHeaderNavBarRequest('formulas', e)}>
+                  Fórmulas
+                </a>
+                <a href="#results" onClick={(e) => this.changeHeaderNavBarRequest('results', e)}>
+                  Resultados
+                </a>
+                <a href="#categorys" onClick={(e) => this.changeHeaderNavBarRequest('categorys', e)}>
+                  Categorías
+                </a>
+                <a href="#items" onClick={(e) => this.changeHeaderNavBarRequest('items', e)}>
+                  Items de proyectos
+                </a>
+                <a href="#features" onClick={(e) => this.changeHeaderNavBarRequest('features', e)}>
+                  Features
+                </a>
+                <a href="#uom" onClick={(e) => this.changeHeaderNavBarRequest('uom', e)}>
+                  UOM
+                </a>
               </div>
-            </Container>
+              <div className="hidden lg:flex items-center space-x-4">
+                <span className="font-semibold text-red-500">{role ? role : ''}</span>
+                <a href="#assign_pf" onClick={(e) => this.changeHeaderNavBarRequest('assign_pf', e)}>
+                  Asignar Validadores
+                </a>
+                <a href="#validators" onClick={(e) => this.changeHeaderNavBarRequest('validators', e)}>
+                  Validadores
+                </a>
+                <button onClick={(e) => this.handleChangeObjectElement()} className="text-red-500">
+                  Sign Out
+                </button>
+              </div>
+            </div>
           </nav>
         );
       }
