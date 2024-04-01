a1c7aac8a90718e14de50851f64251bc0a00a25c
Jairock93 <jairojas.r12@gmail.com>
2024-04-01 07:48:10 -0500

    ajuste menu

--- src/components/views/Navbars/HeaderNavbar.jsx
+++ src/components/views/Navbars/HeaderNavbar.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
-// Tailwind
+import React, { Component } from 'react';
+//Tailwind
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react';
+
+import { Auth } from 'aws-amplify';
+
 // Import images
-import LOGO from "components/common/_images/suan_logo.png";
-import { Auth } from "aws-amplify";
-import s from "components/Constructor/Navbar/HeaderNavbar.module.css";
+import LOGO from '../../common/_images/suan_logo.png';
+import s from './HeaderNavbar.module.css';
 
-export default function CombinedHeaderNavbar() {
-  let role = localStorage.getItem("role");
+export default class HeaderNavbar extends Component {
+  async logOut() {
+    await Auth.signOut();
+    window.location.href = '/';
+    localStorage.removeItem('role');
+  }
 
-  const handleSignOut = async () => {
-    try {
-      await Auth.signOut();
-      localStorage.removeItem("role");
-      window.location.href = window.location.pathname;
-    } catch (error) {
-      console.log("error signing out: ", error);
-    }
-  };
-
-  const findLastAuthUserKey = () => {
+  findLastAuthUserKey() {
     for (let key in localStorage) {
       if (key.includes("CognitoIdentityServiceProvider") && key.includes(".LastAuthUser")) {
         const userlog = localStorage[key];
@@ -27,53 +23,59 @@ export default function CombinedHeaderNavbar() {
       }
     }
     return null;
-  };
-  
-  let userlog = findLastAuthUserKey();
-  
-  return (
-    <Navbar className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <NavbarBrand href="/" style={{ marginLeft: "2%" }}>
-          <img
-            src={LOGO}
-            height="40"
-            width="auto"
-            className="d-inline-block align-top w-10"
-            alt="ATP"
-          />
-        </NavbarBrand>
-        <NavbarToggle />
-        <NavbarCollapse>
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            {role === 'constructor' && (
-              <>
-                <NavbarLink onClick={() => (window.location.href = "/constructor")}>Mis Proyectos</NavbarLink>
-                <NavbarLink onClick={() => (window.location.href = "/new_project")}>Postular proyecto</NavbarLink>
-                {/* <NavbarLink onClick={() => (window.location.href = "/creating_wallet")}>¿Cómo crear tu billetera?</NavbarLink> */}
-              </>
-            )}
-            {role === 'validator' && (
-              <>
-                <NavbarLink onClick={() => (window.location.href = "/validator_admon")}>Proyectos asignados</NavbarLink>
-              </>
-            )}
-            {localStorage.getItem('role') ? (
-              <div>
-                <button className={s.signing} onClick={() => handleSignOut()}>Desconectar</button>
-                <button className='role'>
-                  {userlog}<br />
-                  <p className='role_btn'>
-                    {role === 'validator' ? 'Validador' : (role === 'constructor' ? 'Propietario' : role)}
-                  </p>
-                </button>
+  }
+
+  render() {
+    let role = localStorage.getItem('role');
+    let userlog = this.findLastAuthUserKey();
+    return (
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src={LOGO} className="h-8" alt="ATP" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Suan</span>
+            </a>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                {role === 'admon' && <li><a href="/admon" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Administrar</a></li>}
+                {role === 'investor' && <li><a href="/investor_admon" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Perfil</a></li>}
+                {role === 'validator' && <li><a href="/validator_admon" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Perfil</a></li>}
+                {role === 'constructor' && <li><a href="/constructor" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Perfil</a></li>}
+                {localStorage.getItem('role') ?
+                  <li>
+                    <div>
+                      <button className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 dark:bg-transparent">Desconectar</button>
+                      <button className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 dark:bg-transparent">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#fff">
+                          <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/>
+                        </svg>
+                        {role}
+                      </button>
+                    </div>
+                  </li>
+                : 
+                <> 
+                <li><a href="#tecnologia" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Tecnología</a></li>
+                      <li><a href="#porque" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">¿Por qué Suan?</a></li>
+                      <li><a href="https://marketplace.suan.global/" target="_blank" rel="noreferrer" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Proyectos</a></li>
+                      <li><button className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 dark:bg-transparent" onClick={() => window.location.href="/login"}>Ingresar</button></li>
+                      </>
+                }</ul>
               </div>
-            ) : (
-              <button className={s.signing} onClick={() => (window.location.href = "/login")}>Conectar</button>
-            )}
-          </ul>
-        </NavbarCollapse>
-      </div>
-    </Navbar>
-  );
+              </div>
+            </nav>
+     )
+  }
 }
