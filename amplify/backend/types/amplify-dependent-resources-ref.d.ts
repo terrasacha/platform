4033eab30f2c9f970d4970c760e037aadee066fd
LoOW3 <ignaciodiaznanni@gmail.com>
2024-09-12 17:10:00 -0300

    new amplify app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "kiosuanbcrjsapp": {
+    "platform": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
   },
   "auth": {
-    "kiosuanbcrjsappa6accc93": {
+    "platformauth": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
@@ -18,7 +18,7 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "storage": {
-    "s37845b3b9": {
+    "platformstorage": {
       "BucketName": "string",
       "Region": "string"
     }
