3bb4ae60649c9e3f4677930b76b396fdb941e2d9
Neider Smith <neider.smith1@gmail.com>
2025-04-02 04:12:07 -0500

    Last changes Checkpoint

--- src/components/Admon/Products/ListProducts.jsx
+++ src/components/Admon/Products/ListProducts.jsx
@@ -422,7 +422,7 @@ export default class ListProducts extends Component {
                 <th>Eliminar</th>
                 <th>Nombre</th>
                 <th>Categoria</th>
-                <th>Estadp</th>
+                <th>Estado</th>
                 <th>Descripción</th>
                 <th>Imagen</th>
                 <th>Características</th>
@@ -712,7 +712,7 @@ export default class ListProducts extends Component {
               </Modal.Header>
               <Modal.Body>
                 <ToastContainer />
-                <Table striped hover size="sm" borderless>
+                <Table striped bordered hover responsive borderless>
                   <thead>
                     <tr>
                       <th>Feature ID</th>
@@ -758,7 +758,7 @@ export default class ListProducts extends Component {
               </Modal.Body>
               <Modal.Footer>
                 <Button onClick={(e) => this.handleHideModalProductFeatures(e)}>
-                  Close
+                  Cerrar
                 </Button>
               </Modal.Footer>
             </Modal>
--- src/components/views/landingPage/PQRForm.jsx
+++ src/components/views/landingPage/PQRForm.jsx
@@ -55,7 +55,6 @@ export default class PQRForm extends Component {
         const bucketUrl = `https://${bucketName}.s3.${region}.amazonaws.com/public`;
         const fileUrl = `${bucketUrl}/${result.key}`;
 
-
         // Asignar URL a la variable correcta según el tipo de archivo
         if (prqFile.type === "application/pdf") {
           pdfUrl = fileUrl;
@@ -81,7 +80,7 @@ export default class PQRForm extends Component {
     };
 
     try {
-      const response = await fetch(process.env['REACT_APP_URL_API_PQR'], {
+      const response = await fetch(process.env["REACT_APP_URL_API_PQR"], {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -164,8 +163,12 @@ export default class PQRForm extends Component {
                 minLength="30"
                 placeholder="Describe detalladamente tu consulta"
               />
-              <Form.Text className="text-muted">
-                La descripción debe tener al menos 30 caracteres.
+
+              <Form.Text>
+                <div className="flex justify-between text-muted text-sm m-0">
+                  <p>La descripción debe tener al menos 30 caracteres.</p>
+                  <p>{prqDescription.length} caracteres</p>
+                </div>
               </Form.Text>
             </Form.Group>
             <Form.Group controlId="prqFile" className="mb-3">
